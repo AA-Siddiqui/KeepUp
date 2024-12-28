03c090208876040f860a5c9621893a60e0f447d7
Addan Abdullah <addanabdullahsiddiqui@gmail.com>
2024-12-29 00:44:27 +0500

    auth wasnt working because String.fromEnvironement expects the .env file through the cli argument and i wasnt building with it. yup, the file being absent didnt stop the compilation nor did it crash the program and it did not even give a warning. welcome to hell ig

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -246,12 +246,11 @@ class _LoginPageState extends State<LoginPage> {
     }
 
     try {
-      final res = await Supabase.instance.client.auth.signInWithIdToken(
+      await Supabase.instance.client.auth.signInWithIdToken(
         provider: OAuthProvider.google,
         idToken: idToken,
         accessToken: accessToken,
       );
-      throw Exception(res);
     } on Exception catch (e) {
       if (mounted) {
         ScaffoldMessenger.of(context).showSnackBar(
