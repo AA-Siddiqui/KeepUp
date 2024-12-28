8b50bad1891c03190eae52654aaca4325d6b4045
Addan Abdullah <addanabdullahsiddiqui@gmail.com>
2024-12-28 23:45:34 +0500

    full page and navbar working

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -30,98 +30,108 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              {
-                "icon": Icons.home,
-                "label": "Home",
-              },
-              {
-                "icon": Icons.article,
-                "label": "Tasks",
-              },
-              {
-                "icon": Icons.settings,
-                "label": "Settings",
-              },
-            ].indexed.map((indexedItem) {
-              final index = indexedItem.$1;
-              final item = indexedItem.$2;
-
-              return GestureDetector(
-                onTap: () => setState(() {
-                  _pageTabIndex = index;
-                  _pageController.animateToPage(
-                    _pageTabIndex,
-                    duration: const Duration(milliseconds: 250),
-                    curve: Curves.easeInOut,
-                  );
-                }),
-                child: Container(
-                  padding: const EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    color: _pageTabIndex == index ? Colors.redAccent : null,
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(item["icon"] as IconData),
-                      Text(item["label"] as String),
-                    ],
-                  ),
-                ),
-              );
-            }).toList() as List<Widget>,
-          ),
-        ),
-      )
-          .animate(
-            controller: _navBarAnimController,
-          )
-          .slideY(
-            delay: _navBarTimeout,
-            begin: 0,
-            end: 1,
-            curve: Curves.decelerate,
-          ),
-      body: PageView(
-        controller: _pageController,
-        onPageChanged: (newIndex) {
-          setState(() {
-            _pageTabIndex = newIndex;
-            _navBarAnimController.reverse().whenComplete(() {
-              _navBarAnimController.forward();
-            });
-          });
-        },
+      body: Stack(
         children: [
-          Container(
-            color: Colors.amberAccent,
-            child: const Center(
-              child: Text("Page 1"),
-            ),
+          PageView(
+            controller: _pageController,
+            onPageChanged: (newIndex) {
+              setState(() {
+                _pageTabIndex = newIndex;
+                _navBarAnimController.reverse().whenComplete(() {
+                  _navBarAnimController.forward();
+                });
+              });
+            },
+            children: [
+              Container(
+                color: Colors.amberAccent,
+                child: const Center(
+                  child: Text("Page 1"),
+                ),
+              ),
+              Container(
+                color: Colors.redAccent,
+                child: const Center(
+                  child: Text("Page 2"),
+                ),
+              ),
+              Container(
+                color: Colors.blueAccent,
+                child: const Center(
+                  child: Text("Page 3"),
+                ),
+              ),
+            ],
           ),
-          Container(
-            color: Colors.redAccent,
-            child: const Center(
-              child: Text("Page 2"),
-            ),
-          ),
-          Container(
-            color: Colors.blueAccent,
-            child: const Center(
-              child: Text("Page 3"),
-            ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            left: 0,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(15, 0, 15, 15),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: const EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    {
+                      "icon": Icons.home,
+                      "label": "Home",
+                    },
+                    {
+                      "icon": Icons.article,
+                      "label": "Tasks",
+                    },
+                    {
+                      "icon": Icons.settings,
+                      "label": "Settings",
+                    },
+                  ].indexed.map((indexedItem) {
+                    final index = indexedItem.$1;
+                    final item = indexedItem.$2;
+
+                    return GestureDetector(
+                      onTap: () => setState(() {
+                        _pageTabIndex = index;
+                        _pageController.animateToPage(
+                          _pageTabIndex,
+                          duration: const Duration(milliseconds: 250),
+                          curve: Curves.easeInOut,
+                        );
+                      }),
+                      child: Container(
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color:
+                              _pageTabIndex == index ? Colors.redAccent : null,
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(item["icon"] as IconData),
+                            Text(item["label"] as String),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList() as List<Widget>,
+                ),
+              ),
+            )
+                .animate(
+                  controller: _navBarAnimController,
+                )
+                .slideY(
+                  delay: _navBarTimeout,
+                  begin: 0,
+                  end: 1,
+                  curve: Curves.decelerate,
+                ),
           ),
         ],
       ),
